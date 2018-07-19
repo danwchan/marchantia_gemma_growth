f2c8f447fd591df837157b2897ced411f3e929c3
danwchan <dchan@intouchsol.com>
2018-07-19 14:56:33 -0400

    wrangle the data

--- voth_reanalysis.R
+++ voth_reanalysis.R
@@ -19,15 +19,65 @@ require(dplyr)
 require(tidyr)
 require(stringr)
 require(ggplot2)
+require(tibble)
+#datatable
 
 #+ session-info
 sessionInfo() #for reproducibility
 
+#' #Reading in and Tidying Data
+#' 
+#' There are two csv files which contian hand transcribed observations from the two papers I will be reanalyzing. A description of these files can be found in the github README and/or inside the comments attached to the code.
+
+#+ data-processing , message = FALSE
+#this part of the first csv just has some information to make the process of making solutions easier
+salts_data <- read_csv("Voth_data/solutions.csv") %>%
+  select(salt, molar_mass)
+
+#the next part has the information taken from Table 1 in each paper
 solutions_data <- read_csv("Voth_data/solutions.csv") %>%
-  transform(paperID = as.integer(paperID)) %>%
+  select(-molar_mass) %>%
+  gather(solution, molarity, 8:19) %>% #tidy it up!
+  filter(molarity != 0) %>% #and remove out the rows which will have no impact in calcualting concentrations
+  separate(solution, into = c("mixID", "paperID"), sep = "-", convert = TRUE) %>% #these will be used to join this table to the working_data table
+  arrange(paperID, mixID, salt)
   
+#
+concentration_calculation <- read_csv("Voth_data/growth_data.csv") %>%
+  rowid_to_column("experiment") %>% #this is because of the way I will be calculating the ion concentrations via a join to the solutions_data table... I don't think this is the "right" way to do things
+  transmute(experiment = experiment,
+           paperID = paperID,
+           K_mix = K_mix / mix_total_ratio,
+           Ca_mix = Ca_mix / mix_total_ratio,
+           Mg_mix = Mg_mix / mix_total_ratio,
+           NO3_mix = NO3_mix / mix_total_ratio,
+           PO4_mix = PO4_mix / mix_total_ratio,
+           SO4_mix = SO4_mix / mix_total_ratio) %>%
+  gather(mixID, ratio, -experiment, -paperID) %>%
+  left_join(solutions_data, by = c("mixID" = "mixID", "paperID" = "paperID")) %>%
+  mutate(K_conc = K_molar * molarity * ratio,
+         Ca_conc = Ca_molar * molarity * ratio,
+         Mg_conc = Mg_molar * molarity * ratio,
+         NO3_conc = NO3_molar * molarity * ratio,
+         PO4_conc = PO4_molar * molarity * ratio,
+         SO4_conc = SO4_molar * molarity * ratio) %>%
+  group_by(experiment) %>%
+  summarise(K_conc = sum(K_conc),
+            Ca_conc = sum(Ca_conc),
+            Mg_conc = sum(Mg_conc),
+            NO3_conc = sum(NO3_conc),
+            PO4_conc = sum(PO4_conc),
+            SO4_conc = sum(SO4_conc))
 
 working_data <- read_csv("Voth_data/growth_data.csv") %>%
+  rowid_to_column("experiment") %>% #this is because of the way I will be the concentrations were calculated in a separate table (concent)
+  left_join(concentration_calculation, by = "experiment") %>%
+  mutate(gemma_cups = gemma_cups / n_plants, # calculate the plant per plant instead of per group (average)
+         area = area / n_plants,
+         dry_weight = dry_weight / n_plants,
+         weight = weight / n_plants)
+
+
 #do we want these as factors?  
 #working_data$paperID <- parse_factor(working_data$paperID, c("Voth and Hammer 1940", "Voth 1941"))
 #working_data$sex <- parse_factor(working_data$sex, c("male", "female"))
